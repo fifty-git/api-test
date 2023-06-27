f27e40614ab2edf9ed77a77403c3eeac02eeff32
John Aguirre <jdaguirre@telconet.ec>
2023-06-26 22:28:10 -0500

    I added the basic configuration for Express + Logger

---
+++ app.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const serverless = require('serverless-http');
+const express = require('express');
+const logger = require('./logger/logger');
+
+const app = express();
+const port = process.env.PORT ?? 3000;
+
+app.get('/', (req, res) => {
+  res.send(`Hello World!`);
+});
+
+app.listen(port, () => {
+  logger.info(`API running on http://localhost:${port}`);
+});
+
+module.exports.handler = serverless(app);
---
+++ logger/logger.js
@@ -0,0 +1,17 @@
+const winston = require('winston');
+
+// Define the logger configuration
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    winston.format.printf(({ timestamp, level, message }) => {
+      return `[${timestamp}] ${level.toUpperCase()}: ${message}`;
+    })
+  ),
+  transports: [
+    new winston.transports.Console()
+  ],
+});
+
+module.exports = logger;
